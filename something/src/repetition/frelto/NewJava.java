2cb87b821d6d7eb939abb1f8ae8d7d4263c72d2e
Aman Dubey <dubeyaman157@gmail.com>
2025-07-20 21:58:37 +0530

    added one new file

---
+++ something/src/repetition/frelto/NewJava.java
@@ -0,0 +1,12 @@
+package repetition.frelto;
+
+
+public class NewJava {
+
+
+    public static void main(String[] args)
+    {
+        System.out.println("Hello there my name is aman dubey");
+    }
+
+}
